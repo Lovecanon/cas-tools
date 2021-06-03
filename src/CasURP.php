a4e06a986309eae279c681acc4c6eb933864bc11
xiangkai.liao <liao.xiangkai@datatom.com>
2021-06-03 17:31:39 +0800

    fix: update urp ack

--- src/CasURP.php
+++ src/CasURP.php
@@ -21,18 +21,22 @@ class CasURP extends AbstractCasTools {
         parent::__construct($this->api, $auth, $logFile, $logLevel);
     }
 
-    function sync(array $roleArray) {
+    function sync(array $roleArray, array $guids) {
+        if ($guids == null) {
+            $guids = [];
+        }
         $data = [
             "stats" => $this->getSysInfo(),
-            "role" => $roleArray
+            "role" => $roleArray,
+            "ack" => $guids
         ];
 
         $this->logger->info("");
-        $this->logger->info("------------ start ------------");
+        $this->logger->info("------------ sync start ------------");
         $this->logger->info(json_encode($data));
         $ret = $this->post($data);
         $this->logger->info(json_encode($ret));
-        $this->logger->info("------------ end ------------");
+        $this->logger->info("------------ sync end ------------");
         $this->logger->info("");
         return $ret;
     }
